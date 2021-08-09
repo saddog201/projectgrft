fa48a59805d77430cc6e7f733f4650e8b022ac47
saddog201 <84288482+saddog201@users.noreply.github.com>
2021-08-09 14:03:46 -0400

    Update main.js
    People just put in their names and what they do

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 let membersInfo = [ // All the members
-    {name: "Sreeni Eadara", bio: "Actually know wtf is going on"},
-    {name: "Test Test ", bio: "Actually know wtf is going on"},
+    {name: "Sreeni Eadara", bio: "Description"},
+    {name: "First Last", bio: "Description"},
+    {name: "First Last", bio: "Description"},
+    {name: "First Last", bio: "Description"},
+    {name: "First Last", bio: "Description"},
+    {name: "First Last", bio: "Description"},
 ]
 
 let member_cards = document.getElementById("member-cards"); // member-cards Element
@@ -11,14 +15,16 @@ membersInfo.forEach((member => {
     if (!member.name || !member.bio) return false;
 
     member_cards.innerHTML += `
-        <div class="col s12 m6 l3" style=" display: block; padding-right: 1px;width: 33.33%">
-          <div class = 'card' >
-            <div class='header'>
-              <h3> ${member.name} </h3>
-            </div>
-            <div class = 'content'>
-              <p> ${member.bio} </p>
-            </div>
+        <div class="col s12 m6 l3" style=" display: block; padding-left: 10px; width: 33.33%;">
+          <center>
+            <div class = 'card'>
+              <div class= 'header'>
+                <h5> ${member.name} </h5>
+              </div>
+              <div class = 'content'>
+                <p> ${member.bio} </p>
+              </div>
+            </center>
           </div>
         </div>`
 }))
